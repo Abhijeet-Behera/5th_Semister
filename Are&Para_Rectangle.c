98363d22b2cc4176409a66884afb3259e1700fc6
Abhijeet-Behera <132600619+Abhijeet-Behera@users.noreply.github.com>
2025-09-13 16:57:24 +0530

    Add files via upload

---
+++ Are&Para_Rectangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define pie 3.14
+int main(){
+    double Radius,Area,length,breadth,peri;
+    printf("Lenght&Breadth: ");
+    scanf("%lf%lf",&length,&breadth);
+    Area=length*breadth;
+    peri=2*length*breadth;
+
+    printf("Area: %f\n",Area);
+    printf("Perimeter: %f",peri);
+    return 0;
+}
